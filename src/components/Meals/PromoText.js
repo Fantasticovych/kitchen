c2c6f1a9f15cc22da641f2df8e55d9e0a67332a9
Valeriy <valercka2305@gmail.com>
2025-04-06 19:16:04 +0300

    add meal list

---
+++ src/components/Meals/PromoText.js
@@ -0,0 +1,23 @@
+import styles from "./PromoText.module.css";
+
+const PromoText = () => {
+  return (
+    <section className={styles["promo-text"]}>
+      <h2>Online restaurant</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipis icing elit. Saepe fugiat
+        harum, dolor repellendus dolores nulla facilis placeat! Enim quaerat
+        quae id magni corrupti dolorem praesentium, quasi deleniti possimus
+        omnis debitis.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe fugiat
+        harum, dolor repellendus dolores nulla facilis placeat! Enim quaerat
+        quae id magni corrupti dolorem praesentium, quasi deleniti possimus
+        omnis debitis.
+      </p>
+    </section>
+  );
+};
+
+export default PromoText;
