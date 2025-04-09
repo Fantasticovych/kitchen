6ff33b19c0ff0139b15d60512a1b8a0e24ba3bff
Valeriy <valercka2305@gmail.com>
2025-04-10 00:41:29 +0300

    displaying items in the shopping cart

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,19 +1,44 @@
+import { useRef, useState } from "react";
 import styles from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
 
 const MealItemForm = (props) => {
-  return(
-    <form className={styles.form}>
-      <Input label ='Number' input = {{
-        id:props.id,
-        type:'number',
-        min:'1',
-        step:'1',
-        defaultValue:'1'
-      }}  />
+  const [isAmountValid, setIsAmountValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const inputAmount = amountInputRef.current.value;
+    if (
+      inputAmount.trim().length === 0 ||
+      +inputAmount < 1 ||
+      +inputAmount > 10
+    ) {
+      setIsAmountValid(false);
+      return;
+    }
+
+    props.onAddToCart(+inputAmount);
+  };
+
+  return (
+    <form className={styles.form} onSubmit={submitHandler}>
+      <Input
+        ref={amountInputRef}
+        label="Number"
+        input={{
+          id: props.id,
+          type: "number",
+          min: "1",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
       <button>Add</button>
+      {!isAmountValid && <p>enter the number from 1 to 10 </p>}
     </form>
-  )
+  );
 };
 
 export default MealItemForm;
