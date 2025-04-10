6808c31cc0ef246ecd640890ac1145cf631892c8
Valeriy <valercka2305@gmail.com>
2025-04-10 22:30:44 +0300

    buttonIsAnimetad

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,36 @@
-import { useContext } from "react";
+import { useContext,useState,useEffect} from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [isbuttonIsAnimetad,setIsbuttonIsAnimetad] = useState(false);
+
   const cartContext = useContext(CartContext);
   const cartItemsNumber = cartContext.items.reduce((currentValue, item) => {
     return currentValue + item.amount;
   }, 0);
 
+
+  const buttonClasses =`${styles.button} ${isbuttonIsAnimetad ?styles.bump:''}`;
+
+  useEffect(()=>{   
+    if(cartContext.items.length === 0){
+      return;
+    }
+    setIsbuttonIsAnimetad(true);
+   const timer =  setTimeout(() =>
+    {
+      setIsbuttonIsAnimetad(false)
+    },300)
+
+    return()=>{
+      clearTimeout(timer);
+    }
+  },[cartContext.items]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
